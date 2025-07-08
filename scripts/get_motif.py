4a095cb10f334d7153df1974e124c6b522c3d2e0
zeyak <zeynep.hitay.akdeniz@gmail.com>
2025-07-08 14:37:33 +0200

    get_motif

---
+++ scripts/get_motif.py
@@ -0,0 +1,18 @@
+from glycowork.motif.annotate import annotate_dataset
+from glycowork.glycan_data.loader import df_glycan, df_species
+import pandas as pd
+
+def count_motifs_per_species(df_glycan: pd.DataFrame,
+                             condense: bool = True) -> pd.DataFrame:
+    df_sp = df_glycan["Species"].apply(lambda x: str(x).split(",")[0].strip())
+    df_gly = df_glycan["glycan"]
+    df_motif = annotate_dataset(df_gly.tolist(),
+                                condense=condense)
+    df_motif.index = df_sp.values
+    df_sp_motif_counts = df_motif.groupby(df_motif.index
+                                          ).sum()
+    return df_sp_motif_counts
+
+df_sp_motif_counts = count_motifs_per_species(df_glycan)
+print(df_sp_motif_counts.head())
+
